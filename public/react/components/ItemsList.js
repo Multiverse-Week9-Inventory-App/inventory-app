22483e613023f58f5271c63b4c4bb952569e40bf
PatrickEngelbert2 <patrickengelbert2@gmail.com>
2022-06-03 13:06:05 -0500

    Deploying food for JB

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -5,6 +5,11 @@ import apiURL from "../api";
 export function ItemsList() {
   const [items, setItems] = useState([]);
   const [updateItem, setUpdateItem] = useState(false);
+  const [numRows, setNumRows] = useState(0);
+
+  const countItemsBy3 = () => {
+    setNumRows(Math.ceil(items.length / 3));
+  };
 
   async function fetchItems() {
     try {
@@ -19,13 +24,39 @@ export function ItemsList() {
 
   useEffect(() => {
     fetchItems();
+    countItemsBy3();
   }, [updateItem]);
+
+  // const listItems = () => {
+  //   let result = [];
+  //   for (let i = 0; i < numRows; i++) {
+  //     let thisRowItems;
+  //     let thisRow = <div>{thisRowItems}</div>
+  //     for (let j = 0; j < items.length; j++) {
+  //       // thisRowItems.push()
+  //       result.push(
+  //         thisRowItems.map((item, idx) => {
+  //           return (
+  //             <Items
+  //               item={item}
+  //               setItems={setItems}
+  //               setUpdateItem={setUpdateItem}
+  //               updateItem={updateItem}
+  //               key={idx}
+  //             />
+  //           );
+  //         })
+  //       );
+  //     }
+  //   }
+
+  //   return result.map((row) => {});
+  // };
 
   return (
     <>
       <h2>Item List</h2>
-      <div className="container">
-          {items.map((item, idx) => {
+      <div className="container flexrow">{items.map((item, idx) => {
             return (
               <Items
                 item={item}
@@ -35,8 +66,7 @@ export function ItemsList() {
                 key={idx}
               />
             );
-          })}
-      </div>
+          })}</div>
     </>
   );
 }
