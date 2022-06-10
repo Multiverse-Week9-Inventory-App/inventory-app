4f0fd2204a79371eccbba24d2750055098b7c6f5
PatrickEngelbert2 <patrickengelbert2@gmail.com>
2022-06-10 11:48:09 -0500

    added embeded intro video and component for it

--- server/app.js
+++ server/app.js
@@ -17,7 +17,6 @@ app.use(express.json());
 // serve up static files (e.g. html and css files)
 app.use(express.static(path.join(__dirname, "../dist")));
 
-
 /////////////////////////////////////////////////////////////////////////////
 // app.use(express.static(path.join(__dirname, '..', 'public')))
 
